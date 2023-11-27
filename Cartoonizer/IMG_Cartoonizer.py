f6bc985755b9d2a5a227b7ad1b40b8c0630b9d71
flopox <l.llop@alumno.etec.um.edu.ar>
2023-11-27 15:56:58 -0300

    cartoonizer with opencv libraries

---
+++ Cartoonizer/IMG_Cartoonizer.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+def cartoonize(image_path):
+    # Leer la imagen
+    image = cv2.imread(image_path)
+
+    # Verificar si la imagen se cargó correctamente
+    if image is None:
+        print(f"No se pudo abrir o leer la imagen: {image_path}")
+        return
+
+    # Convertir la imagen a escala de grises
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Ajustar los parámetros de umbral adaptativo para controlar la sensibilidad de los bordes
+    edges = cv2.adaptiveThreshold(gray, 255,
+                                  cv2.ADAPTIVE_THRESH_MEAN_C,
+                                  cv2.THRESH_BINARY, blockSize=15, C=7)
+
+    # Aplicar filtro bilateral para obtener colores suaves
+    color = cv2.bilateralFilter(image, 9, 250, 250)
+
+    # Crear la imagen tipo cartoon
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+    # Mostrar la imagen original y la imagen cartoonizada
+    cv2.imshow("Original Image", image)
+    cv2.imshow("Cartoonized Image", cartoon)
+
+    # Esperar a que se presione una tecla y luego cerrar las ventanas
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# Uso del cartoonizer con una imagen específica
+cartoonize("C:/Path/de/la/imagen/a/cartoonizar/imagen.jpg") 
+# ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ #
+#Cualquier extensioón de archivo relacionada a imagenes, es aceptada.
--- README.md
+++ README.md
@@ -6,6 +6,9 @@ As a Python entry level enthusiast, my primary focus is on personal growth. So I
 
 Simple Calculator: A calculator that can perform basic operations like addition, subtraction, multiplication, and division.
 
+
+Cartoonizer: A Python project using OpenCV to convert regular images into cartoon-style illustrations. It employs techniques like adaptive thresholding and bilateral filtering, offering users a quick and artistic way to transform photos into visually appealing cartoons.
+
 Task List: It involves the ability to add, remove, and mark tasks as complete underscores my commitment to organization and productivity in a daily life.
 
 Password Generator: It is a random password generator that, using letters, digits, punctuation, and Umlaut, can withstand cyber threats, making it difficult for others to guess your password.
